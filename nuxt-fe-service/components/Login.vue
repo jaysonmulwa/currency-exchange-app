60808caaffedbccda33eff5f20e967b8893f0e19
jaysonmulwa <jayson.mulwa@gmail.com>
2022-06-11 18:19:29 +0300

    feat: added login page

--- nuxt-fe-service/components/Login.vue
+++ nuxt-fe-service/components/Login.vue
@@ -14,6 +14,24 @@
 
       </div>
     </div>
+    <div class="container px-5 pt-12 pb-24 mx-auto">
+      <div class="container px-5 py-24 mx-auto flex justify-center ">
+        <div class="lg:w-1/3 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0 relative z-10 shadow-md">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 tracking">Login</h1>
+          <div class="relative mb-4">
+            <label for="username" class="leading-7 text-sm text-gray-600">Username</label>
+            <input type="text" id="username" name="username" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+          <div class="relative mb-4">
+            <label for="password" class="leading-7 text-sm text-gray-600">Password</label>
+            <input type="password" id="password" name="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+          <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login</button>
+          <p class="text-xs text-gray-500 mt-3">Don't have an account?. <NuxtLink to="/signup"><b><u>Sign up</u></b></NuxtLink> here.</p>
+          
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
